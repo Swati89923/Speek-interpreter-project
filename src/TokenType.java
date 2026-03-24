3007168dbd80454dd48539f0a507bd1994d39fd7
Vinay <vinayprajapati0806@gmail.com>
2026-03-24 10:01:43 +0530

    add TokonType.java file

---
+++ src/TokenType.java
@@ -0,0 +1,29 @@
+package speek;
+
+public enum TokenType {
+
+    
+    LET,            
+    BE,             
+    SAY,            
+    IF,             
+    IS_GREATER_THAN,
+    IS_LESS_THAN,  
+    IS_EQUAL_TO,    
+    THEN,           
+    REPEAT,        
+    TIMES,          
+    ELSE,           
+
+    NUMBER,        
+    STRING,        
+    IDENTIFIER,    
+
+    PLUS,          
+    MINUS,         
+    STAR,           
+    SLASH,          
+
+    NEWLINE,        
+    INDENT,         
+    EOF             
